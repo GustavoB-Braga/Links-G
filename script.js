87e1440cbbbc615781a789e9afa8d1fc9b4b7c72
GustavoB-Braga <braga.gustavo0910@gmail.com>
2024-02-21 18:46:00 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+  
+    const image = document.querySelector("#profile img")
+  
+    if (html.classList.contains("light")) {
+      image.setAttribute("src", "assets/avatar-light.png")
+      image.setAttribute(
+        "alt",
+        "Foto de Mayk Brito sorrindo, usando óculos escuro e camisa preta, barba e fundo azul."
+      )
+    } else {
+      image.setAttribute("src", "assets/avatar.png")
+      image.setAttribute(
+        "alt",
+        "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, sem barba e fundo amarelo."
+      )
+    }
+  }
